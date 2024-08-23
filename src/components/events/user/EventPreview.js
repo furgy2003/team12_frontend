af24e94af6353c6ae238d0293c345964bd290242
furgy2003 <furgy2003@gmail.com>
2024-08-24 00:24:38 +0700

    components structure

---
+++ src/components/events/user/EventPreview.js
@@ -0,0 +1,3 @@
+export default function EventPreview() {
+  return <p>EventPreview</p>;
+}
---
+++ src/pages/eventDummy.js
@@ -0,0 +1,12 @@
+import EventPreview from "@/components/events/user/EventPreview";
+import EventPreviewCard from "@/components/events/user/EventPreviewCard";
+import { Fragment } from "react";
+
+export default function EventDummy() {
+  return (
+    <Fragment>
+      <EventPreview />
+      <EventPreviewCard />
+    </Fragment>
+  );
+}
