85600a9991262ce7143b22f336d625843ff35185
JoshNg123 <jnjw.999@gmail.com>
2024-08-24 21:41:23 +0800

    Add Stuff

--- src/pages/admin/layout.js
+++ src/pages/admin/layout.js
@@ -1,9 +1,8 @@
-// pages/admin/layout.js
 import Sidebar from "../../components/admin/sideBar";
 
 export default function AdminLayout({ children }) {
   return (
-    <div className="flex">
+    <div className="flex min-h-screen">
       <Sidebar />
       <main className="flex-1 p-6 bg-white">{children}</main>
     </div>
