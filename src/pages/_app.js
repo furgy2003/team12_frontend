f40a8e4de06720d03c110ebe5c475419213bd619
furgy2003 <furgy2003@gmail.com>
2024-08-24 10:18:15 +0700

    configure color palette

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,27 @@
 import "@/styles/globals.css";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#F9EF1F",
+      },
+      secondary: {
+        main: "#01A9FF",
+      },
+      text: {
+        main: "#585858",
+      },
+      red: {
+        main: "#B23239",
+      },
+    },
+  });
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
