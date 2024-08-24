9cc7e2e8ac1f62de4a55a3b31149f9204e9885a3
Udayveer Singh <theudayvs@gmail.com>
2024-08-24 15:35:42 +0800

    Event Card and Event Card Preview

--- src/components/events/user/EventPreviewCard.js
+++ src/components/events/user/EventPreviewCard.js
@@ -1,3 +1,27 @@
-export default function EventPreviewCard() {
-  return <p>EventPreviewCard</p>;
-}
+import React from 'react';
+import Image from 'next/image'
+import styles from '../../css/EventPreviewCard.module.css';
+
+const EventPreviewCard = ({ imageUrl, date, month, title, link }) => {
+  return (
+    <div style={{ padding: '20px' }}>
+    <div className={styles.card}>
+      <Image src={imageUrl} alt="Event" className={styles.cardImage} />
+      <div className={styles.cardContent}>
+        <div className={styles.cardDate}>
+          <h2>{date}</h2>
+          <span>{month}</span>
+        </div>
+        <div className={styles.cardDetails}>
+          <h3>{title}</h3>
+          <a href={link} className={styles.cardLink}>
+            Click for more detail
+          </a>
+        </div>
+      </div>
+    </div>
+    </div>
+  );
+};
+
+export default EventPreviewCard;
--- src/pages/eventDummy.js
+++ src/pages/eventDummy.js
@@ -1,12 +1,29 @@
 import EventPreview from "@/components/events/user/EventPreview";
 import EventPreviewCard from "@/components/events/user/EventPreviewCard";
+import eventimage from '../../public/images/1.jpg'
 import { Fragment } from "react";
 
 export default function EventDummy() {
+  const handleRegister = () => {
+    // Empty Function 
+  }
   return (
     <Fragment>
-      <EventPreview />
-      <EventPreviewCard />
+      <EventPreview 
+        imageSrc={eventimage}
+        title="Kayaking with your fellow friends"
+        date="Sun, July 28, 2028"
+        time="10:00 AM"
+        location="Sham Shui Po"
+        description="Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+        onRegister={handleRegister}/>
+      <EventPreviewCard 
+       imageUrl={eventimage}
+       date="28"
+       month="JUL"
+       title="Kayaking with your fellow friends"
+       link="#"
+       />
     </Fragment>
   );
 }
