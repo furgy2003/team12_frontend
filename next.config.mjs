8e24120a300c8d44aa0348acd4137a67c25e8a85
furgy2003 <90049379+furgy2003@users.noreply.github.com>
2024-08-25 21:42:32 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,7 @@ const nextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: 'http://localhost:8080/api/:path*',
+        destination: 'http://localhost:5000/api/:path*',
         // destination:
         //   "https://team12-backend-code-to-give-ca637a425bb3.herokuapp.com:5000/api/:path*",
       },
