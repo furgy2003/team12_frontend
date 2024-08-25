3e42f338f14094e3c05abf5186290721d71a7073
Andy123qq4 <ksyung@connect.ust.hk>
2024-08-25 18:59:32 +0800

    CORS

--- next.config.mjs
+++ next.config.mjs
@@ -7,8 +7,7 @@ const nextConfig = {
       {
         source: "/api/:path*",
         // destination: 'http://localhost:8080/api/:path*',
-        destination:
-          "https://sleepy-woodland-35400-5cdfb9a5b9a7.herokuapp.com/api/:path*",
+        destination: 'https://team12-backend-code-to-give-ca637a425bb3.herokuapp.com/api/:path*',
       },
     ];
   },
